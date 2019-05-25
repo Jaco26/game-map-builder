14a917a68f61797e7c3fc7bbf97a8ab7a8d6cf25
Jacob Albright <jacob.albright23@gmail.com>
2019-05-25 10:54:48 -0500

    likely fixed bug with 'this' by using 'definite assignmnent assertion'. reorganized 'grid' store module

--- src/components/sandbox/sb-canvas.vue
+++ src/components/sandbox/sb-canvas.vue
@@ -4,6 +4,7 @@
       ref="my-canvas"
       :height="height"
       :width="width"
+      @click="getTile"
     ></canvas>
     <slot></slot>
   </div>
@@ -11,61 +12,43 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { GridTile } from '@/store/modules/grid/types'
+import { State, Mutation, namespace } from 'vuex-class'
 import { Component, Watch } from 'vue-property-decorator'
-import { State, Mutation, namespace } from 'vuex-class';
-import { MutationMethod } from 'vuex';
-import { GridTile } from '@/store/grid'
-
-const gridModule = namespace('grid');
-
+const gridMod = namespace('grid')
 @Component
-export default class SbCanvas extends Vue {
-  // constructor() {
-  //   super();
-  //   console.log(this)
-  // }
-  isMousedown: boolean = false;
+export default class SBCanvas extends Vue {
 
-  @gridModule.State grid: [];
+  // VUEX
+  // state
+  @gridMod.State grid!: [];
+  @gridMod.State width!: number;
+  @gridMod.State height!: number;
+  @gridMod.State cols!: number;
+  @gridMod.State rows!: number;
+  @gridMod.State selected!: GridTile;
+  // mutations
+  @gridMod.Mutation GENERATE_GRID: any
+  @gridMod.Mutation SIZE_TILES: any
+  @gridMod.Mutation SET_SELECTED: any;
 
-  @gridModule.State width: number;
-  @gridModule.State height: number;
-
-  @gridModule.State cols: number;
-  @gridModule.State rows: number;
-  @gridModule.State selected: GridTile;
-
-  @Watch('selected')
-  onSelectedChange(newVal: GridTile, oldVal: GridTile | null) {
-    if (oldVal) console.log(oldVal.color)
-    this.clearRect();
-    this.drawGrid()
+  // METHODS
+  getTile(e: MouseEvent): void {
+    const vm = this;
+    const rowIndex = Math.floor(e.offsetY / (vm.height / vm.rows));
+    const colIndex = Math.floor(e.offsetX / (vm.width / vm.cols));
+    vm.SET_SELECTED({ colIndex, rowIndex });
   }
-
-  @gridModule.Mutation GENERATE_GRID: any
-  @gridModule.Mutation SIZE_TILES: any
-  @gridModule.Mutation SET_SELECTED: any;
-
-  // getTile(e: MouseEvent) {
-  //   const vm = this;
-  //   const rowIndex = Math.floor(e.offsetY / (vm.height / vm.rows));
-  //   const colIndex = Math.floor(e.offsetX / (vm.width / vm.cols));
-  //   vm.SET_SELECTED({ colIndex, rowIndex });
-  // }
-
-  clearRect() {
+  clearRect(): void {
     if (!this.ctx) return;
     this.ctx.clearRect(0, 0, this.width, this.height);
   }
-
-  draw(cb: Function) {
+  draw(cb: Function): void {
     if (!this.ctx) return;
     this.ctx.beginPath();
     cb(this.ctx);
     this.ctx.closePath();
   }
-
-
   drawGrid() {
     this.grid.forEach((row: GridTile[]) => {
       row.forEach((tile: GridTile) => {
@@ -78,25 +61,27 @@ export default class SbCanvas extends Vue {
       });
     });
   }
-
-  mounted() {
-    const c: any = this.$refs['my-canvas']
-    this.GENERATE_GRID({ rows: 10, cols: 10 })
-    this.SIZE_TILES();
-    this.drawGrid();
-  }
-
+  
+  // COMPUTED
   get ctx(): CanvasRenderingContext2D | null {
     const canvas: any = this.$refs['my-canvas'];
     if (canvas) return canvas.getContext('2d');
     return null;
   }
 
-  onMousemove(e: any) {
-    this.draw((ctx: CanvasRenderingContext2D) => {
-      ctx.strokeRect(e.offsetX, e.offsetY, 10, 10);
-    });
+  // WATCH
+  @Watch('selected')
+  onSelectedChange(newVal: GridTile, oldVal: GridTile | null) {
+    this.clearRect();
+    this.drawGrid()
   }
 
-};
+  // LIFECYCLE
+  mounted() {
+    const c: any = this.$refs['my-canvas']
+    this.GENERATE_GRID({ rows: 10, cols: 10 })
+    this.SIZE_TILES();
+    this.drawGrid();
+  }
+}
 </script>
---
+++ src/store/modules/grid/types.ts
@@ -0,0 +1,26 @@
+export class GridTile {
+  row: number;
+  col: number;
+
+  x: number = 0;
+  y: number = 0;
+  width: number = 0;
+  height: number = 0;
+
+  color: string = '';
+  outline: string = '';
+
+  constructor(row: number, col: number) {
+    this.row = row;
+    this.col = col;
+  }
+}
+
+export interface GridState {
+  width: number;
+  height: number,
+  cols: number,
+  rows: number,
+  grid: GridTile[][],
+  selected: GridTile | null,
+}
